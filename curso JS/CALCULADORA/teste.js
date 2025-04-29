fd0d937bcac32df83403ba99fa42c31286c893c3
Anderson D Bernardo <Anderson.danttas@hotmail.com>
2025-04-29 16:37:06 -0300

    CALCULADORA

--- curso JS/CALCULADORA/teste.js
+++ curso JS/CALCULADORA/teste.js
@@ -1,26 +1,3 @@
-let vetor = [1, 2, 3, '*', 4, 5, 6]
-let n1 = ''
-let n2 = ''
+let vetor = []
 
-for(let pos in vetor) {
-    if (vetor.indexOf('+') > pos) {
-        n1 += vetor[pos]
-    }
-    else if (vetor.indexOf('-') > pos) {
-        n1 += vetor[pos]
-    }
-    else if (vetor.indexOf('*') > pos) {
-        n1 += vetor[pos]
-    }
-    else if (vetor.indexOf('/') > pos) {
-        n1 += vetor[pos]
-    }
-    else {
-        pos++
-        n2 += vetor[pos]
-    }
-
-}
-
-console.log(n1)
-console.log(n2)
+console.log(vetor.length)
