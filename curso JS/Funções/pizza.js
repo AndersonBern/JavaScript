7d2a0f72a6de9c63816c1f6294a8f1a42c5c53b3
Anderson D Bernardo <anderson.danttas@hotmail.com>
2025-05-20 17:55:10 -0300

    att

---
+++ curso JS/Funções/pizza.js
@@ -0,0 +1,3 @@
+const pizza = (tipoDeQueijo, tipoDeRecheio) => {console.log('A pizza é de '+ tipoDeQueijo + ' com ' + tipoDeRecheio)}
+
+pizza('mussarela', 'frango')
---
+++ curso JS/Funções/quadrado.js
@@ -0,0 +1,5 @@
+let lista = [1, 2, 3, 4, 5]
+
+const quadrado = lista.map(numero => numero * numero);
+
+console.log(quadrado)
